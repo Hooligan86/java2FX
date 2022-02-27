ba89dcdc8376e0329d97205179f1ee6fabbe4739
Hooligan <listat25@gmail.com>
2022-02-27 22:36:09 +0200

    main

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.hooligan.homeworksfx {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.hooligan.homeworksfx to javafx.fxml;
+    exports com.hooligan.homeworksfx;
+}
