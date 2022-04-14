e1b205cdc02a4fdf08748877c943c675071c1654
Hooligan <listat25@gmail.com>
2022-04-14 18:48:59 +0300

    Fix

--- src/main/java/server/authentication/AuthenticationService.java
+++ src/main/java/server/authentication/AuthenticationService.java
@@ -3,8 +3,6 @@ package server.authentication;
 public interface AuthenticationService {
     String getUsernameByLoginAndPassword(String login, String password);
 
-//    void createUser(String login, String password, String username);
-//    Boolean checkLoginByFree(String login);
     void startAuthentication();
     void endAuthentication();
 }
--- src/main/java/server/authentication/DBAuthenticationService.java
+++
@@ -1,4 +0,0 @@
-package server.authentication;
-
-public class DBAuthenticationService {
-}
