b86424c9fa3e40a5b99d46d66908fbf3c48250b1
Hooligan <listat25@gmail.com>
2022-04-14 18:34:40 +0300

    test

--- src/main/java/server/authentication/AuthenticationService.java
+++ src/main/java/server/authentication/AuthenticationService.java
@@ -2,6 +2,9 @@ package server.authentication;
 
 public interface AuthenticationService {
     String getUsernameByLoginAndPassword(String login, String password);
+
+//    void createUser(String login, String password, String username);
+//    Boolean checkLoginByFree(String login);
     void startAuthentication();
     void endAuthentication();
 }
